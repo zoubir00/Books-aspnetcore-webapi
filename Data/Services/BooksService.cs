587674247393f3d183639798033fb41bbebbbb54
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-14 12:03:58 +0100

    Get books with author name

--- Data/Services/BooksService.cs
+++ Data/Services/BooksService.cs
@@ -14,7 +14,23 @@ namespace My_Books.Data.Services
         }
 
         public List<Book> GetAllBooks() => _context.Books.ToList();
-        public Book GetBookById(int bookId) => _context.Books.FirstOrDefault(n=>n.Id==bookId)!;
+        public BookWithAuthorsVM GetBookById(int bookId)
+        {
+            var _bookWithAuthors = _context.Books.Where(n=>n.Id==bookId).Select(book => new BookWithAuthorsVM()
+            {
+                Title = book.Title,
+                Description = book.Description,
+                IsRead = book.IsRead,
+                DateRead = book.IsRead ? book.DateRead.Value : null,
+                Rate = book.IsRead ? book.Rate.Value : null,
+                Genre = book.Genre,
+                CoverUrl = book.CoverUrl,
+                publisherName = book.Publisher.Name,
+                AuthorsName = book.Book_Authors.Select(n => n.Author.FullName).ToList()
+
+            }).FirstOrDefault();
+            return _bookWithAuthors;
+        }
 
         public Book UpdateBook(int bookId,BookVM book)
         {
--- Data/ViewModels/BookVM.cs
+++ Data/ViewModels/BookVM.cs
@@ -12,4 +12,17 @@
         public int publisherId { get; set; }
         public List<int> AuthorsIds { get; set; }
     }
+
+    public class BookWithAuthorsVM
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public int? Rate { get; set; }
+        public string Genre { get; set; }
+        public string CoverUrl { get; set; }
+        public string publisherName { get; set; }
+        public List<string> AuthorsName { get; set; }
+    }
 }
