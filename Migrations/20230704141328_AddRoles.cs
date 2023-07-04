1e4d3e49c10dec1686128eed510089c48637ecba
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-07-04 16:44:04 +0100

    seeding the roles

---
+++ Migrations/20230704141328_AddRoles.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace My_Books.Migrations
+{
+    public partial class AddRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id","Name", "NormalizedName", "ConcurrencyStamp" } ,
+                values:new object[] {Guid.NewGuid().ToString(),"Admin","Admin",Guid.NewGuid().ToString()}
+                );
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id","Name", "NormalizedName", "ConcurrencyStamp" } ,
+                values:new object[] {Guid.NewGuid().ToString(),"User","User",Guid.NewGuid().ToString()}
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from AspNetRoles");
+        }
+    }
+}
